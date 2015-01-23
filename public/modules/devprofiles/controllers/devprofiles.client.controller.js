69a06efa896d2fcd3c67b838e50e341156599ffb
Asael Arenas <asael2@gmail.com>
2015-01-22 21:32:59 -0500

    Autocomplete maps implemented

--- public/modules/devprofiles/controllers/devprofiles.client.controller.js
+++ public/modules/devprofiles/controllers/devprofiles.client.controller.js
@@ -74,102 +74,56 @@ angular.module('devprofiles').controller('DevprofilesController', ['$scope', '$s
 
 		$scope.setLocation = function(){
 			
-			$scope.clickedLocation = function(){
-				$scope.showInputLocation = true;
-				// console.log("clickedLocation");
-			}
+			
 
-			$scope.getKeyLocation = function(keyCode){
-				// console.log(document.getElementById("pac-input").value );
-				$scope.location = document.getElementById("pac-input").value;
-				if (keyCode == '13'){
-					$scope.showInputLocation = false;
-				}
-			}
+			// $scope.getKeyLocation = function(keyCode){
+			// 	// console.log(document.getElementById("pac-input").value );
+				
+			// 	//$scope.location = document.getElementById("pac-input").value;
+			// 	if (keyCode == '13'){
+			// 		$scope.showInputLocation = false;
+			// 	}
+			// }
 			
 			$scope.blurLocation = function(){
 				$scope.showInputLocation = false;
 				// console.log( $scope.location);
 			}
 			
-			// Google places
-
-			var markers = [];
-
-			// var map = new google.maps.Map(document.getElementById('map-canvas'), {
-			// 	mapTypeId: google.maps.MapTypeId.ROADMAP
-			// });
-
-			var mapOpt = {
-				center:new google.maps.LatLng(40,-75),
-				zoom:12,
-				mapTypeId:google.maps.MapTypeId.ROADMAP
-			};
-			var map=new google.maps.Map(document.getElementById("map-canvas"),mapOpt);
+			$scope.clickedLocation = function(){
+				$scope.showInputLocation = true;
+				// console.log("clickedLocation");
+				// $scope.devLocation = place.formatted_address;
 			
-
-			// var defaultBounds = new google.maps.LatLngBounds(
-			//   new google.maps.LatLng(-33.8902, 151.1759),
-			//   new google.maps.LatLng(-33.8474, 151.2631) );
-
-			// map.fitBounds(defaultBounds);
-
-			// Create the search box and link it to the UI element.
-			var input = (document.getElementById('pac-input'));
-			// map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-			var searchBox = new google.maps.places.SearchBox(input);
-
-			// Listen for the event fired when the user selects an item from the
-			// pick list. Retrieve the matching places for that item.
-			google.maps.event.addListener(searchBox, 'places_changed', function() {
-				
-				var places = searchBox.getPlaces();
-
-				if (places.length == 0) {
-				  return;
-				}
-				for (var i = 0, marker; marker = markers[i]; i++) {
-				  marker.setMap(null);
-				}
-
-				// For each place, get the icon, place name, and location.
-				markers = [];
-				var bounds = new google.maps.LatLngBounds();
-
-				for (var i = 0, place; place = places[i]; i++) {
-					var image = {
-					url: place.icon,
-					size: new google.maps.Size(71, 71),
-					origin: new google.maps.Point(0, 0),
-					anchor: new google.maps.Point(17, 34),
-					scaledSize: new google.maps.Size(25, 25)
-					};
-
-					// Create a marker for each place.
-					var marker = new google.maps.Marker({
-					map: map,
-					icon: image,
-					title: place.name,
-					position: place.geometry.location
-					});
-
-					markers.push(marker);
-
-					bounds.extend(place.geometry.location);
-
-				}
-
-				map.fitBounds(bounds);
-
+			// Google Maps
+			var initialOpts = {
+				center:new google.maps.LatLng(0,0),
+				zoom:0,
+				mapTypeControl: false,
+				navigationControl: false,
+				disableDefaultUI: true,
+                // disableDoubleClickZoom:true,
+                draggable: false,
+                streetViewControl:false,
+                mapTypeId:google.maps.MapTypeId.TERRAIN
+			};
+			var map = new google.maps.Map(document.getElementById("map-canvas"), initialOpts);
+			// Google Autocomplete
+			var input = (document.getElementById('pac-input'));	
+			var options = {
+			  types: ['(cities)']
+			};
+			var autocomplete = new google.maps.places.Autocomplete(input, options);
+			google.maps.event.addListener(autocomplete, 'place_changed', function() {	
+				var place = autocomplete.getPlace();
+				map.setCenter(place.geometry.location);
+				map.setZoom(11);
+				$scope.devLocation = place.formatted_address;
+				$scope.showInputLocation = false;
+				console.log(place.formatted_address);
+				// debugger;
 			});
-
-			// Bias the SearchBox results towards places that are within the bounds of the
-			// current map's viewport.
-			google.maps.event.addListener(map, 'bounds_changed', function() {
-				var bounds = map.getBounds();
-				searchBox.setBounds(bounds);
-			});
+			}	
 		}
 
 	}
